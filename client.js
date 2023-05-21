9fc452cc8ca29ca3df120b20a788a0befc3c5e87
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-05-21 17:21:11 -0500

    move up command added to client.js file

--- client.js
+++ client.js
@@ -9,6 +9,7 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: ADK");
+    conn.write("Move: up");
   });
 
   conn.on("data", (data) => {
